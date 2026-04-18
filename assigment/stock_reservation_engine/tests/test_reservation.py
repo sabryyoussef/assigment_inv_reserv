a00c0e718f2b2cc17f74514dfafaa263ed16ee9f
sabry_youssef <vendorah2@gmail.com>
2026-04-18 03:55:24 +0000

    fix: use detailed_type for storable products in tests (Odoo 17+)

--- assigment/stock_reservation_engine/tests/test_reservation.py
+++ assigment/stock_reservation_engine/tests/test_reservation.py
@@ -15,13 +15,13 @@ class TestStockReservation(TransactionCase):
         cls.uom_unit = cls.env.ref('uom.product_uom_unit')
         cls.product = cls.env['product.product'].create({
             'name': 'Reservation Product',
-            'type': 'storable',
+            'detailed_type': 'storable',
             'uom_id': cls.uom_unit.id,
             'uom_po_id': cls.uom_unit.id,
         })
         cls.lot_product = cls.env['product.product'].create({
             'name': 'Lot Reservation Product',
-            'type': 'storable',
+            'detailed_type': 'storable',
             'tracking': 'lot',
             'uom_id': cls.uom_unit.id,
             'uom_po_id': cls.uom_unit.id,
